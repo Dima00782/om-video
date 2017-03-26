ace37bd3404e9093469f7461b07832b475dde42c
Dima00782 <Dima00782@gmail.com>
2017-03-26 16:48:32 +0700

    add MSE

--- src/main/java/org/red5/omvideo/SignalConnection.java
+++ src/main/java/org/red5/omvideo/SignalConnection.java
@@ -7,9 +7,7 @@ import org.mp4parser.streaming.output.mp4.FragmentedMp4Writer;
 import org.red5.logging.Red5LoggerFactory;
 import org.slf4j.Logger;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -18,10 +16,12 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -34,23 +34,45 @@ public class SignalConnection {
 	
 	private static final Logger LOG = Red5LoggerFactory.getLogger(SignalConnection.class, "omvideo");
 
-	private static final String VIDEO_FILE_PATH = "/home/dmitry/code/om-video/small.mp4";
+	private static final String VIDEO_FILE_PATH = "/home/dmitry/Downloads/Avengers2.mp4";
+
+	private static final ArrayList<Session> clients = new ArrayList<>();
 	
     @OnOpen
-    public void onOpen(Session session) throws IOException, URISyntaxException {
+    public void onOpen(final Session session) throws IOException {
+        LOG.info("User connected");
         ClassicMp4ContainerSource classicMp4ContainerSource = new ClassicMp4ContainerSource(
-                new URI("http://video.blendertestbuilds.de/download.php?file=download.blender.org/peach/trailer_480p.mov").toURL().openStream()
+                new FileInputStream(new File(VIDEO_FILE_PATH))
         );
         List<StreamingTrack> streamingTracks = classicMp4ContainerSource.getTracks();
-        File f = new File("/home/dmitry/code/om-video/new_small.mp4");
-        FragmentedMp4Writer writer = new FragmentedMp4Writer(streamingTracks, new FileOutputStream(f).getChannel());
+        FragmentedMp4Writer writer = new FragmentedMp4Writer(streamingTracks, new WritableByteChannel() {
+            @Override
+            public boolean isOpen() {
+               return session.isOpen();
+           }
+
+            @Override
+            public void close() throws IOException {
+                session.close();
+            }
+
+            @Override
+            public int write(ByteBuffer src) throws IOException {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                int size = src.remaining();
+                session.getBasicRemote().sendBinary(src);
+                return size;
+            }
+        });
 
         LOG.info("Reading and writing started.");
         classicMp4ContainerSource.call();
-        writer.close();
-        LOG.info(f.getAbsolutePath());
 
-        LOG.info("User connected");
+        writer.close();
     }
 
     @OnMessage
--- src/main/webapp/client.js
+++ src/main/webapp/client.js
@@ -1,15 +1,39 @@
 const connection = new WebSocket('ws://localhost:5080/omvideo/ws');
-
 const videoTag = document.querySelector("#video-tag");
+const mimeCodec = 'video/mp4; codecs="avc1.64001E, mp4a.40.2"';
+
+let mediaSource = null;
+let sourceBuffer = null;
+
+function getMediaSource() {
+	let mediaSource = null;
+	if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
+		mediaSource = new MediaSource;
+        videoTag.src = URL.createObjectURL(mediaSource);
+        mediaSource.addEventListener('sourceopen', () => {
+        	sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
+        });
+    } else {
+    	console.error('Unsupported MIME type or codec: ', mimeCodec);
+    }
+
+    return mediaSource;
+}
+
+function startVideo() {
+	mediaSource = getMediaSource();
+	videoTag.play();
+}
 
 connection.onopen = function () {
   console.log("Connected");
+  startVideo();
 }
 
 connection.onmessage = function (message) {
   console.log("Got message", message.data);
-
-  // TODO
+  sourceBuffer.appendBuffer(new Uint8Array(message));
+  videoTag.play();
 };
 
 connection.onerror = function (err) {
